3fd813782575c30609989e5797dda687c7d3312b
Nhomar Hernandez <nhomar@gmail.com>
2013-02-21 16:54:30 -0430

    [ADD] Gcode maps commented

--- web_gmaps/__openerp__.py
+++ web_gmaps/__openerp__.py
@@ -35,6 +35,7 @@ Just allow given a point with this format X,Y in decimal coordinates, show the m
     'depends': ['web'],
     'js': [
         'static/src/js/lib.js', 
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyDo-1vLyCKoMU1JkZZSe2Z9xpknaB0d6Qc&sensor=true',
     ],
     'css': [
     ],
--- web_gmaps/static/src/js/lib.js
+++ web_gmaps/static/src/js/lib.js
@@ -1,4 +1,4 @@
-openerp.web_url = function(instance) {
+openerp.web_gmaps = function(instance) {
     var _t = instance.web._t,
         _lt = instance.web._lt,
         widget_name = 'gmaps';
@@ -16,37 +16,30 @@ openerp.web_url = function(instance) {
         },
         
         render_value: function() {
+            console.log('Entre al render');
             if (!this.get("effective_readonly")) {
                 this._super();
             } else {
                 var tmp = this.get('value');
-                if (!this.isUrl(tmp)) {
-                    tmp = instance.webclient.session.server + this.get('value');
+////                //if (this.UrlExists(tmp)){
+////                    //Magia de renderizado
+////                     this.$el.find('div.oe_map_canvas').text("Hola Mundo");
+//                //} else {
+//                //    this.$el.find('a').attr('href', instance.webclient.session.server + '/web_url/static/html/404.html').text('Link is broken verify destiny.');
+//                //}
+//                console.log(google);
+//                var mapOptions = {
+//                  center: new google.maps.LatLng(10.505833, -66.914444),
+//                  zoom: 8,
+//                  disableDefaultUI: true,
+//                  overviewMapControl: true,
+//                  scaleControl: true,
+//                  mapTypeId: google.maps.MapTypeId.SATELLITE
+//                };
+//                var map = new google.maps.Map(this.$el.find('div.oe_map_canvas'),
+//                    mapOptions);
                 }
-                if (this.UrlExists(tmp)){
-                    this.$el.find('a').attr('href', tmp).text(this.get('value') ? tmp : '');
-                } else {
-                    this.$el.find('a').attr('href', instance.webclient.session.server + '/web_url/static/html/404.html').text('Link is broken verify destiny.');
-                }
-            }
-        },
-
-        isUrl: function (url) {
-        	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-	        return regexp.test(url);
-        },
-
-        UrlExists: function(url) {
-            var http = new XMLHttpRequest();
-            var hn = url.replace("http://","").replace("https://","").replace("ftp://","").split("/")[0];
-            if (location.host === hn) {                
-                http.open('HEAD', url, false);
-                http.send();
-                return http.status!=404;
-            } else {
-                return true;
-                console.log('Are Not the same');
-            }
+//            },
         },
     });
 }
