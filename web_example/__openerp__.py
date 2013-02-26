f8ef485f5f421f2b89a72036afa6d9ec35f6c306
OscarAlca <oszckar@gmail.com>
2013-02-26 11:31:53 -0600

    Map added, this approach is a starting pont to some view like mail module, a complete view just for the map

--- web_example/__openerp__.py
+++ web_example/__openerp__.py
@@ -3,5 +3,9 @@
 'description':"Basic example of a (Future) web module",
 'category':'Hidden',
 'depends':['web'],
+'data':['web_example.xml'],
+'js':['static/src/js/first_module.js'],
+'css':['static/src/css/web_example.css'],
+#'qweb':['static/src/xml/web_example.xml'],
 }
 
