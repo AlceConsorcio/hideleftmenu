ec04a7f616096018e7783a5260c4b7af79df815b
OscarAlca <oszckar@gmail.com>
2013-03-07 14:06:15 -0600

    Google Key added to de JS code, the calculation of the lat and lng is calculated based on the address on the partner form

--- web_gmaps/static/src/js/lib.js
+++ web_gmaps/static/src/js/lib.js
@@ -2,7 +2,7 @@ openerp.web_gmaps = function(instance) {
     var _t = instance.web._t,
         _lt = instance.web._lt,
         widget_name = 'gmaps';
-    //Declare the widget
+    //Declare the widget - Add the widget to the widget collection of the form widget
     instance.web.form.widgets.add(widget_name, 'instance.web.form.Gmaps'); 
     //instance of the widget itself
     instance.web.form.Gmaps = instance.web.form.FieldChar.extend({
@@ -12,7 +12,15 @@ openerp.web_gmaps = function(instance) {
         display_name: _lt('Field Gmaps'),
         
         initialize_content: function() {
+            var script = document.createElement("script");
             this._super();
+            script.type = "text/javascript";
+            script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBwNE-vFDyyOb62ODaRiqpiL2kz8wR0aTc&sensor=true&callback=gmap_draw"
+            window.gmap_draw = function(){
+                console.log("Dunno wtd here");
+            }
+            $("head").append(script);
+
         },
         
         render_value: function() {
@@ -25,7 +33,7 @@ openerp.web_gmaps = function(instance) {
                   zoom: 20,
                   mapTypeId: google.maps.MapTypeId.ROADMAP
                 }, 
-                    torender = this.$el.find('div.oe_map_canvas');
+                torender = this.$el.find('div.oe_map_canvas');
                 var map = new google.maps.Map(torender[0], mapOptions);
             }
         },
