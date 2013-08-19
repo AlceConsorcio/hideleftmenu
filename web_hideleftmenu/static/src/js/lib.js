b4974792c75f062f8d6f0eb498463fb67b71af23
Nhomar Hernandez <nhomar@gmail.com>
2013-08-18 21:41:44 -0430

    [REM] COnsole.log removed

--- web_hideleftmenu/static/src/js/lib.js
+++ web_hideleftmenu/static/src/js/lib.js
@@ -1,5 +1,4 @@
 openerp.web_hideleftmenu = function (instance) {
-    console.log("Module loaded");
     instance.web.WebClient.include({
         events: {
             'click .oe_hidemenu': 'hideleftmenu',
