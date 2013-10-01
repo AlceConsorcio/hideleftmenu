5c19fee6127e8b75583908eff63fb6435d015787
Nhomar Hernandez <nhomar@gmail.com>
2013-09-30 20:51:19 -0430

    [ADD] Controller to ensure all uploaded data in messages goes to the same directory. [IMP] New controller add timestamp to filename to avoid duplicate names in the same group TODO: remove this and ask if i want to use the existent one. [ADD] Mail group folder autocreated with data.

---
+++ portal_public_documents/controllers/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from web import http
+from openerp.addons.web.controllers.main import Binary
+openerpweb = http
+import simplejson
+import time
+import openerp
+import os
+import StringIO
+import xmlrpclib
+import base64
+
+class Binary(Binary):
+    @openerpweb.httprequest
+    def upload_attachment(self, req, callback, model, id, ufile):
+        Model = req.session.model('ir.attachment')
+        ModelXML = req.session.model('ir.model.data')
+        group_folder_id = ModelXML.get_object_reference(
+                'portal_public_documents',
+                'portal_public_mail_groups_folder')
+        out = """<script language="javascript" type="text/javascript">
+                    var win = window.top.window;
+                    win.jQuery(win).trigger(%s, %s);
+                </script>"""
+        try:
+            import datetime
+            attachment_id = Model.create({
+                'name': str(time.time()).replace('.','') + ' ' + ufile.filename,
+                'datas': base64.encodestring(ufile.read()),
+                'datas_fname': ufile.filename,
+                'res_model': model,
+                'res_id': int(id),
+                'parent_id': group_folder_id[1],
+            }, req.context)
+            args = {
+                'filename': ufile.filename,
+                'id':  attachment_id
+            }
+        except xmlrpclib.Fault, e:
+            args = {'error':e.faultCode }
+        return out % (simplejson.dumps(callback), simplejson.dumps(args))
