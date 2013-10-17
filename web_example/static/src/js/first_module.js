b7a5fb80e0a1ccfe23b6dfabbf2d128254637a32
Nhomar - Vauxoo <nhomar@gmail.com>
2013-10-16 23:50:12 -0430

    [IMP] all the algorithm is a widget

--- web_example/static/src/js/first_module.js
+++ web_example/static/src/js/first_module.js
@@ -1,86 +1,89 @@
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwNE-vFDyyOb62ODaRiqpiL2kz8wR0aTc&sensor=true&callback=gmap_draw"
+$("head").append(script);
+window.gmap_draw = function(){
+}
 openerp.web_example = function (instance){
-
-var map, polygon;
-var markers = [];
-var path = new google.maps.MVCArray;
-var event_click_map;
-
-$.fn.clicktoggle = function(a, b) {
-    return this.each(function() {
-        var clicked = false;
-        $(this).click(function() {
-            if (clicked) {
-                clicked = false;
-                return b.apply(this, arguments);
-            }
-            clicked = true;
-            return a.apply(this, arguments);
-        });
-    });
-};
-
-function writeArea() {
-    var area = google.maps.geometry.spherical.computeArea(polygon.getPath());
-    $("#shape_area").html(area + " m<sup>2</sup>");
-    
-    $("#paths").html("");
-    for (var i = 0; i < path.length; ++i)
-    {
-        $("#paths").html($("#paths").html() + "<br />" + path.getAt(i)); 
-    }
-}
-
-function addPoint(event) {
-    path.insertAt(path.length, event.latLng);
-
-    var marker = new google.maps.Marker({
-        position: event.latLng,
-        map: map,
-        draggable: true,
-        animation: "BOUNCE"
-    });
-    markers.push(marker);
-    marker.setTitle("#" + path.length);
-
-    google.maps.event.addListener(marker, 'click', function() {
-        marker.setMap(null);
-        var i = 0;
-        for (var I = markers.length; i < I && markers[i] != marker; ++i); 
-        markers.splice(i, 1);
-        path.removeAt(i);
-        
-        writeArea();
-    });
-    
-    google.maps.event.addListener(marker, 'dragend', function() {
-        var i = 0;
-        for (var I = markers.length; i < I && markers[i] != marker; ++i);
-        path.setAt(i, marker.getPosition());
-        
-        writeArea();
-    });
-    
-    writeArea();
-}
-
-function startShape() {
-    polygon.setPath(new google.maps.MVCArray([path]));
-    event_click_map = google.maps.event.addListener(map, 'click', addPoint);
-}
-
-function endShape() {
-    google.maps.event.removeListener(event_click_map);
-}
-
-        
 
     instance.web_example.Map = instance.web.Widget.extend({
         template: 'web_example.Map',
         init: function(parent) {
 			this._super(parent);
 		},
-        start: function () {
-			this._super();
+        writeArea: function () {
+            this.area = google.maps.geometry.spherical.computeArea(this.polygon.getPath());
+            $("#shape_area").html(this.area + " m<sup>2</sup>");
+            
+            $("#paths").html("");
+            for (var i = 0; i < this.path.length; ++i)
+            {
+                $("#paths").html($("#paths").html() + "<br />" + this.path.getAt(i)); 
+            }
+        },
+        addPoint: function(event, parent){
+            self = this;
+            parent.path.insertAt(this.path.length, event.latLng);
+
+            var marker = new google.maps.Marker({
+                position: event.latLng,
+                map: this.map,
+                draggable: true,
+                animation: "BOUNCE"
+            });
+            parent.markers.push(marker);
+            marker.setTitle("#" + this.path.length);
+
+            google.maps.event.addListener(marker, 'click', function() {
+                marker.setMap(null);
+                var i = 0;
+                for (var I = markers.length; i < I && markers[i] != marker; ++i); 
+                markers.splice(i, 1);
+                self.path.removeAt(i);
+                
+                writeArea();
+            });
+            
+            google.maps.event.addListener(marker, 'dragend', function() {
+                var i = 0;
+                for (var I = markers.length; i < I && markers[i] != marker; ++i);
+                self.path.setAt(i, marker.getPosition());
+                
+                this.writeArea();
+            });
+            
+            this.writeArea();
+        },
+        startShape: function() {
+            self = this;
+            this.polygon.setPath(new google.maps.MVCArray([self.path]));
+            this.event_click_map = google.maps.event.addListener(this.map, 'click', function(e){
+                self.addPoint(e, self)
+            });
+        },
+        endShape: function() {
+            google.maps.event.removeListener(this.event_click_map);
+        },
+        loadMap: function(parent){
+            self = this;
+            this.map, this.polygon;
+            this.markers = [];
+            parent.path = new google.maps.MVCArray;
+            this.event_click_map;
+
+            $.fn.clicktoggle = function(a, b) {
+                return this.each(function() {
+                    var clicked = false;
+                    $(this).click(function() {
+                        if (clicked) {
+                            clicked = false;
+                            return b.apply(this, arguments);
+                        }
+                        clicked = true;
+                        return a.apply(this, arguments);
+                    });
+                });
+            };
 			
             var mapCoords = new google.maps.LatLng(21.150975, -101.645336);
             var mapOptions = { 
@@ -88,15 +91,9 @@ function endShape() {
 				zoom:17, 
 				mapTypeId: google.maps.MapTypeId.ROADMAP 
 			}
-            //console.log(map);
-            //this._super();
-            //var thing = this.$el.text("Hello, world!"); 
-            var torender = this.$el;
+            var torender = this.$el.find('#map_canvas');
 
-            //console.log(thing);
-            map = new google.maps.Map(torender[0], mapOptions);
-            //console.log(mapped);
-            //thing.text(mapped);
+            this.map = new google.maps.Map(torender[0], mapOptions);
             
             var polygonOptions = { 
 				strokeColor: "#ff0000",//color,
@@ -105,23 +102,26 @@ function endShape() {
 				fillColor: "#ff0000",//color,
 				fillOpacity: 0.35 
 			}
-			polygon = new google.maps.Polygon(polygonOptions);
-			polygon.setMap(map);
+			this.polygon = new google.maps.Polygon(polygonOptions);
+			this.polygon.setMap(this.map);
 
-
-			torender.after("<div id='shape_b' class='unselected'></div><div id='shape_area'></div><div id='paths'></div>");
 			$("#shape_b").clicktoggle(
 				function () {
 					$(this).removeClass("unselected");
 					$(this).addClass("selected");
-					startShape();
+					self.startShape();
 				},
 				function () {
 					$(this).removeClass("selected");
 					$(this).addClass("unselected");
-					endShape();
+					self.endShape();
 				}
 			);
+        },
+        start: function() {
+            var self = this;
+			this._super.apply(this, arguments);
+            this.$('a.oe_load_map').on('click', function(){self.loadMap(self)});
         }
     });
     instance.web.client_actions.add('example.action','instance.web_example.Map');
